8e84dea5adc4a672ec2dc661f0bad319e8ddba2d
MrUnwonted <arjungodzarjun@gmail.com>
2023-05-09 10:01:48 +0530

    Constatnts

--- ShopmeCommon/src/main/java/com/shopme/common/Constants.java
+++ ShopmeCommon/src/main/java/com/shopme/common/Constants.java
@@ -1,13 +1,38 @@
 package com.shopme.common;
 
+//public class Constants {
+//	public static final String S3_BASE_URI;
+//
+//	static {
+//		String bucketName = System.getenv("AWS_BUCKET_NAME");
+//		String region = System.getenv("AWS_REGION");
+//		String pattern = "https://%s.s3.%s.amazonaws.com";
+//
+//		S3_BASE_URI = bucketName == null ? "" : String.format(pattern, bucketName, region);
+//	}
+//}
+
+import java.util.Map;
+
 public class Constants {
+
 	public static final String S3_BASE_URI;
-	
+
 	static {
+
+		Map<String, String> env = System.getenv();
+		for (String key : env.keySet())
+		{
+			System.out.println(key + ":" + env.get(key));
+		}
+
 		String bucketName = System.getenv("AWS_BUCKET_NAME");
+		System.out.println("bucket Name: " + bucketName);
 		String region = System.getenv("AWS_REGION");
+		System.out.println("Region : " + region);
 		String pattern = "https://%s.s3.%s.amazonaws.com";
-		
-		S3_BASE_URI = bucketName == null ? "" : String.format(pattern, bucketName, region);
+		S3_BASE_URI = bucketName == null ? "" : String.format(pattern, bucketName, region);;
+		System.out.println("Base URI is: " + S3_BASE_URI);
 	}
+
 }
