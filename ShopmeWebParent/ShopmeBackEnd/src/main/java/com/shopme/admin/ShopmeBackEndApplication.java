fb8d17219ec8901b9439354fdb43612e39cfedf4
MrUnwonted <86820656+MrUnwonted@users.noreply.github.com>
2023-05-19 16:49:28 +0530

    Update ShopmeBackEndApplication.java

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.util.Map;
+
 @SpringBootApplication
 @EntityScan({"com.shopme.common.entity"})
 public class ShopmeBackEndApplication {
