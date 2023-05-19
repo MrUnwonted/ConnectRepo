bf9e69eb8af1062248d78c200903c8c8daac7ad9
MrUnwonted <86820656+MrUnwonted@users.noreply.github.com>
2023-05-19 16:42:23 +0530

    Update ShopmeBackEndApplication.java

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -11,16 +11,17 @@ public class ShopmeBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopmeBackEndApplication.class, args);
 		
+			// Retrieve all environment variables
+       			 Map<String, String> env_var = System.getenv();
+
+       			 // Loop through all environment variables
+			for (String envName : env_var.keySet()) {
+			    // Print environment variable name and value to console
+			    System.out.format("%s=%s", envName, env_var.get(envName));
+			    System.out.println();
+        }
+
 	}
 	
-	// Retrieve all environment variables
-        Map<String, String> env_var = System.getenv();
-
-        // Loop through all environment variables
-        for (String envName : env_var.keySet()) {
-            // Print environment variable name and value to console
-            System.out.format("%s=%s", envName, env_var.get(envName));
-            System.out.println();
-        }
 
 }
